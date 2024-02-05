4eb8b3bdb26d497ba162f6ff1f188cb924754f36
VladimirWayOne <rekin.vladimir@yandex.ru>
2024-02-05 22:07:40 +0300

    Добавлены тесты для создания/изменения пользователя

---
+++ src/main/java/client/UserClient.java
@@ -0,0 +1,47 @@
+package client;
+
+import dto.CreateUserRequest;
+import dto.LoginUserRequest;
+import dto.UpdateUserRequest;
+import io.restassured.response.Response;
+
+public class UserClient extends RestClient{
+
+    public Response createUser(CreateUserRequest createUserRequest) {
+        return getDefaultRequestSpecification()
+                .body(createUserRequest).log().all()
+                .when()
+                .post("/auth/register");
+
+    }
+
+    public Response loginUser(LoginUserRequest loginUserRequest) {
+        return getDefaultRequestSpecification()
+                .body(loginUserRequest)
+                .when()
+                .post("/auth/login");
+    }
+
+    public Response deleteUser(String bearerToken) {
+        return getDefaultRequestSpecification()
+                .auth()
+                .oauth2(bearerToken)
+                .when()
+                .delete("/auth/user");
+    }
+
+    public Response updateUser(String bearerToken, UpdateUserRequest updateUserRequest) {
+        return getDefaultRequestSpecification()
+                .auth()
+                .oauth2(bearerToken)
+                .body(updateUserRequest).log().all()
+                .when()
+                .patch("/auth/user");
+    }
+    public Response updateUserWithoutAuth(UpdateUserRequest updateUserRequest) {
+        return getDefaultRequestSpecification()
+                .body(updateUserRequest).log().all()
+                .when()
+                .patch("/auth/user");
+    }
+}
---
+++ src/main/java/defaults/RestConfig.java
@@ -0,0 +1,5 @@
+package defaults;
+
+public class RestConfig {
+    public static final String BASE_URI = "https://stellarburgers.nomoreparties.site/api";
+}
